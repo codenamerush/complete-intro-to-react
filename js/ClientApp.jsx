9f7b4dd3e5e0d79518f7dc7a920e01319ea9948e
Aarushi <aarushisharda@gmail.com>
2018-06-04 20:18:07 +0530

    Changing to jsx

--- js/ClientApp.jsx
+++ js/ClientApp.jsx
@@ -1,21 +1,32 @@
 import React from 'react';
 import { render } from 'react-dom';
 
-const ce = React.createElement;
-
 const MyTitle = function(props) {
-  return ce('div', null, ce('h1', { style: { color: props.color } }, props.title));
-};
-
-const MyFirstComponent = function() {
-  return ce(
-    'div',
-    { id: 'my-first-component' },
-    ce(MyTitle, { title: 'Game of Thrones', color: 'YellowGreen' }),
-    ce(MyTitle, { title: 'Stranger Things', color: 'GreenYellow' }),
-    ce(MyTitle, { title: 'Rick and Morty', color: 'LimeGreen' }),
-    ce(MyTitle, { title: 'House of Cards', color: 'peru' })
+  // return ce('div', null, ce('h1', { style: { color: props.color } }, props.title));
+  return (
+    <div>
+      <h1 style={{ color: props.color }}>{props.title}</h1>
+    </div>
   );
 };
 
-render(ce(MyFirstComponent), document.getElementById('app'));
+const MyFirstComponent = function() {
+  // return ce(
+  //   'div',
+  //   { id: 'my-first-component' },
+  //   ce(MyTitle, { title: 'Game of Thrones', color: 'YellowGreen' }),
+  //   ce(MyTitle, { title: 'Stranger Things', color: 'GreenYellow' }),
+  //   ce(MyTitle, { title: 'Rick and Morty', color: 'LimeGreen' }),
+  //   ce(MyTitle, { title: 'House of Cards', color: 'peru' })
+  // );
+  return (
+    <div id="my-first-component">
+      <MyTitle title="Stranger Things" color="YellowGreen" />
+      <MyTitle title="Game of Thrones" color="burlywood" />
+      <MyTitle title="Sense8" color="GreenYellow" />
+      <MyTitle title="iZombie" color="peru" />
+    </div>
+  );
+};
+
+render(<MyFirstComponent />, document.getElementById('app'));
