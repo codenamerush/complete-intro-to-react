1ec28d1ad3f394c8f4e5950ebe14171ea6745f64
Aarushi <aarushisharda@gmail.com>
2018-06-06 19:46:10 +0530

    Added routes using HashRouter

---
+++ js/Landing.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Landing = () => (
+	<div className="landing">
+		<h1>videoplayer</h1>
+		<input type="text" placeholder="Search" />
+		<a>or Browse all</a>
+	</div>
+);
+
+export default Landing;
