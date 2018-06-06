df85736b2a7bc7186474bd5be0e56ed66c30325a
Aarushi <aarushisharda@gmail.com>
2018-06-06 18:43:00 +0530

    Changed content, build and configs

--- js/ClientApp.jsx
+++ js/ClientApp.jsx
@@ -1,32 +1,14 @@
 import React from 'react';
 import { render } from 'react-dom';
 
-const MyTitle = function(props) {
-  // return ce('div', null, ce('h1', { style: { color: props.color } }, props.title));
-  return (
-    <div>
-      <h1 style={{ color: props.color }}>{props.title}</h1>
+const App = () => (
+  <div className="app">
+    <div className="landing">
+      <h1>videoplayer</h1>
+      <input type="text" placeholder="Search" />
+      <a>or Browse all</a>
     </div>
-  );
-};
+  </div>
+);
 
-const MyFirstComponent = function() {
-  // return ce(
-  //   'div',
-  //   { id: 'my-first-component' },
-  //   ce(MyTitle, { title: 'Game of Thrones', color: 'YellowGreen' }),
-  //   ce(MyTitle, { title: 'Stranger Things', color: 'GreenYellow' }),
-  //   ce(MyTitle, { title: 'Rick and Morty', color: 'LimeGreen' }),
-  //   ce(MyTitle, { title: 'House of Cards', color: 'peru' })
-  // );
-  return (
-    <div id="my-first-component">
-      <MyTitle title="Stranger Things" color="YellowGreen" />
-      <MyTitle title="Game of Thrones" color="burlywood" />
-      <MyTitle title="Sense8" color="GreenYellow" />
-      <MyTitle title="iZombie" color="peru" />
-    </div>
-  );
-};
-
-render(<MyFirstComponent />, document.getElementById('app'));
+render(<App />, document.getElementById('app'));
