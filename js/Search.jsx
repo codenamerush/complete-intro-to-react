642fa82f23a179c437057afb803bd19d869fb0aa
Aarushi <aarushisharda@gmail.com>
2018-06-07 18:47:10 +0530

    search modified

--- js/Search.jsx
+++ js/Search.jsx
@@ -1,9 +1,45 @@
-import React from 'react';
+import React, { Component } from 'react';
 import ShowCard from './ShowCard';
 import preload from '../data.json';
 
-const Search = () => (
-	<div className="search">{preload.shows.map(show => <ShowCard key={show.imdbID} {...show} />)}</div>
-);
+class Search extends Component {
+	constructor(props) {
+		super(props);
+
+		this.state = {
+			searchTerm: 'this is a debug statement'
+		};
+	}
+	// state = {
+	// 	searchTerm:
+	// }
+	handleSearchTermChange = (event) => {
+  this.setState({ searchTerm: event.target.value });
+}
+	};
+	render() {
+		return (
+			<div className="search">
+				<header>
+					<h1>svideo</h1>
+					<input
+						onChange={this.handleSearchTermChange}
+						value={this.state.searchTerm}
+						type="text"
+						placeholder="Search"
+					/>
+				</header>
+				<div>
+  {preload.shows
+    .filter(
+      show =>
+        `${show.title} ${show.description}`.toUpperCase().indexOf(this.state.searchTerm.toUpperCase()) >= 0
+    )
+    .map((show, index) => <ShowCard {...show} key={show.imdbID} id={index} />)}
+</div>
+			</div>
+		);
+	}
+}
 
 export default Search;
